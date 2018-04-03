74543fbaf13547a625e14c5880ef103c5673fa73
Alexandre Silva <contato@silvalexandre.com>
2018-04-03 16:03:59 -0300

    Add router utilities

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ require('dotenv').load()
 const chalk = require('chalk')
 const express = require('express')
 const app = express()
+const ru = require('./tools/router')
 
 const PORT = process.env.PORT || 8080
 
 require('./app/connect')()
 require('./app/settings')(app)
 
+app.get('/', (req, res) => {
+  return ru.text(res, 'ok')
+})
+
 // 404
 app.all('*', (req, res) => {
-  return res.sendStatus(404)
+  return ru.status(res, 404)
 })
 
 app.listen(PORT, () => {
---
+++ tools/router.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const chalk = require('chalk')
+
+const error = chalk.bold.red
+const warning = chalk.yellow
+
+/**
+ * Handles async functions.
+ * @param {function} fn Function executed.
+ */
+const asyncMdl = fn => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(err => {
+    console.err(error('ASYNC error'), warning(`|- ${err}`))
+    return next(err)
+  })
+}
+
+/**
+ * Send status.
+ * @param {object} res Res object, provided by express.
+ * @param {number} status Status number.
+ */
+const status = (res, status) => {
+  return res.sendStatus(status)
+}
+/**
+ * Send text.
+ * @param {object} res Res object, provided by express.
+ * @param {string} text Text to be sent.
+ * @param {number} status Status number.
+ */
+const text = (res, text, status = false) => {
+  if (status) return res.status(status).send(text)
+  return res.send(text)
+}
+/**
+ * Send json.
+ * @param {object} res Res object, provided by express.
+ * @param {object} data Data do be sent.
+ * @param {string} label Label to be used by data.
+ * @param {number} status Status number.
+ */
+const json = (res, data, label, status) => {
+  const response = {}
+  response[label] = data
+  if (status) return res.status(status).json({ ...response })
+  return res.json({ ...response })
+}
+
+module.exports = {
+  asyncMdl,
+  status,
+  text,
+  json
+}
