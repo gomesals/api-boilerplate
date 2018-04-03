4d9f0d488394c06498e74583d853aecdec525346
Alexandre Silva <contato@silvalexandre.com>
2018-04-03 17:34:49 -0300

    Improve & add router functions

--- tools/router.js
+++ tools/router.js
@@ -20,7 +20,7 @@ const async = (fn, level = false) => (req, res, next) => {
 }
 /**
  * Send status.
- * @param {object} res Res object, provided by express.
+ * @param {object} res Provided by express.
  * @param {number} status Status number.
  */
 const status = (res, status) => {
@@ -28,7 +28,7 @@ const status = (res, status) => {
 }
 /**
  * Send text.
- * @param {object} res Res object, provided by express.
+ * @param {object} res Provided by express.
  * @param {string} text Text to be sent.
  * @param {number} status Status number.
  */
@@ -38,7 +38,7 @@ const text = (res, text, status = false) => {
 }
 /**
  * Send json.
- * @param {object} res Res object, provided by express.
+ * @param {object} res Provided by express.
  * @param {object} data Data do be sent.
  * @param {string} label Label to be used by data.
  * @param {number} status Status number.
@@ -49,10 +49,44 @@ const json = (res, data, label, status) => {
   if (status) return res.status(status).json({ ...response })
   return res.json({ ...response })
 }
+/**
+ * Send an error with json data.
+ * @param {object} res Provided by express.
+ * @param {object} data Error data.
+ */
+const __error = (res, data) =>
+  res.status(data.status).json({ text: data.text, level: data.level })
+/**
+ * Send an error, or json, based on data's content.
+ * @param {object} res Provided by express.
+ * @param {object} data Data object.
+ * @param {string} label Json's label.
+ * @param {number} status Status number.
+ */
+const data = (res, data, label, status) => {
+  if (data.isValid) {
+    return json(res, data.data, label, status)
+  }
+  return __error(res, data)
+}
+/**
+ * Send an error, or status, based on data.isValid.
+ * @param {object} res Provided by express.
+ * @param {object} data Data object.
+ * @param {number} status Response's status.
+ */
+const updated = (res, data, status = 200) => {
+  if (data.isValid) {
+    return res.sendStatus(status)
+  }
+  return __error(res, data)
+}
 
 module.exports = {
   async,
   status,
   text,
-  json
+  json,
+  data,
+  updated
 }
